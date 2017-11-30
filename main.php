fcaf6e7d5bd8e02d72e36c23a9f94fb4ec14183d
Joseph Garcia <joe@leadsnearby.com>
2017-11-30 09:02:11 -0500

    Version Update

--- main.php
+++ main.php
@@ -3,7 +3,7 @@
 Plugin Name: CSAnalytics
 Plugin URI: http://leadsnearby.com
 Description: Plugin to display LeadsNearby Metrics
-Version: 2.1.6.9
+Version: 2.1.7
 Author: LeadsNearby
 Author URI: http://leadsnearby.com
 License: GPLv2
