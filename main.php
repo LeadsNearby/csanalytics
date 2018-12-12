e99816f2b6c6ced9a35eaf5808d03669913ad83f
Andrew Gillingham <andrew@leadsnearby.com>
2018-12-12 13:13:26 -0500

    Bump version number

--- main.php
+++ main.php
@@ -3,7 +3,7 @@
 Plugin Name: CSAnalytics
 Plugin URI: http://leadsnearby.com
 Description: Plugin to display LeadsNearby Metrics
-Version: 3.0.5
+Version: 3.1.0
 Author: LeadsNearby
 Author URI: http://leadsnearby.com
 License: GPLv2
