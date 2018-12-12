ce9e05b61f037cab81c6d9835ee2db01a4988e97
jgleadsnearby <joe@leadsnearby.com>
2018-12-12 06:21:53 -0500

    Rolled back to previous version
    Rolled back to version b9e20fd6a4668f6fa8742602102043d62727416b

--- lib/inc/functions.php
+++ lib/inc/functions.php
@@ -1,14 +1,12 @@
 <?php
-
 	// CSAnalytics Scripts and Style
 	add_action('wp_enqueue_scripts', 'csanalytics_ext_scripts');
 	function csanalytics_ext_scripts()  {
 		if (!is_admin()) {
-			wp_register_script('js-cookie', plugins_url('js/js-cookie.js',__FILE__),'', '', FALSE);
+			wp_register_script('js-cookie', plugins_url('js/js-cookie.js',__FILE__),array(), null, true);
 			wp_enqueue_script('js-cookie');			
 		}		
 	}
-
 	// Google Tag Manager Scripts
 	add_action( 'wp_head', 'csanalytics_gtagmgr_script' );
 	function csanalytics_gtagmgr_script() {
@@ -17,19 +15,14 @@
             echo stripslashes ($dev_tag_mgr);
         }
 	}
-
 	// Convirza DNI Scripts
 	add_action( 'wp_footer', 'csanalytics_dni_script' );
 	function csanalytics_dni_script() { 
-
 	    $dev_conv_profile = get_option('gapi_dev_conv_profile');
             $dev_conv_script =  get_option('gapi_dev_conv_script');
-
             if ($dev_conv_profile <> "" && $dev_conv_script <> ""){	
-
-	        wp_register_script( 'csanalytics-dni', 'https://dni.logmycalls.com/dni.js','', null, FALSE);
+	        wp_register_script( 'csanalytics-dni', 'https://dni.logmycalls.com/dni.js',array('js-cookie'), null, true);
 	        wp_enqueue_script('csanalytics-dni');
-
                 echo stripslashes ($dev_conv_script);
 			
             }
