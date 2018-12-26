659dd7f3f51489c4fe40331d3c715e2d2795d090
Brian <brian@leadsnearby.com>
2018-12-26 14:22:18 -0500

    Updated Settings Panel

--- lib/admin/admin-functions.php
+++ lib/admin/admin-functions.php
@@ -1,18 +1,17 @@
 <?php
 /*******************************
-  Admin Scripts
-********************************/
+Admin Scripts
+ ********************************/
 add_action('admin_menu', 'csanalytics_theme_page');
-function csanalytics_theme_page ()
-{
-	if ( count($_POST) > 0 && isset($_POST['csanalytics_settings']) )
-	{
-		$options = array (
-			'dev_industry',
-			'dev_email',
-			'dev_key',
-			'dev_profile',
-			'dev_tag_mgr',
+function csanalytics_theme_page() {
+    if (count($_POST) > 0 && isset($_POST['csanalytics_settings'])) {
+        $options = array(
+            'dev_industry',
+            'dev_email',
+            'dev_key',
+            'dev_profile',
+            'dev_tag_mgr',
+            'dev_tag_mgr_footer',
             'speed_enable',
             'adwords_enable',
             'dev_adwords_url',
@@ -21,167 +20,148 @@ function csanalytics_theme_page ()
             'dev_conv_script',
             'dev_heatmap_script',
             'dev_heatmap_page',
-			'dev_web_rank',
-			'dev_web_rank_url',
-			'dev_keyword_rank',
-			'dev_client_payment_type',
-			'dev_client_monthly',
-			'dev_client_perlead',
-			'dev_chat',
-			'dev_calls',
-			'dev_citations',
-			'dev_bl_citations',
-			'dev_bl_reviews',
-			'dev_citations',
-			'weather_enable',
-			'weather_api',
-			'weather_zip',
-			'dev_ontraport_enable',
-			'dev_ontraport_clientkey',
-			'dev_ontraport_apikey',
-			'dev_ontraport_appid',
-			'dev_servicetitan_apikey',
-		);
-		
-		foreach ( $options as $opt )
-		{
-			delete_option ( 'gapi_'.$opt, $_POST[$opt] );
-			add_option ( 'gapi_'.$opt, $_POST[$opt] );	
-		}				 
-	}
-	
-	if ( count($_POST) > 0 && isset($_POST['gapi_date_settings']) )
-	{
-		$options = array (
-			'calendar_start',
-			'calendar_end',
-			'calendar_diff',		
-		);
-		
-		foreach ( $options as $opt )
-		{
-			delete_option ( 'gapi_'.$opt, $_POST[$opt] );
-			add_option ( 'gapi_'.$opt, $_POST[$opt] );	
-		}				 
-	}
+            'dev_web_rank',
+            'dev_web_rank_url',
+            'dev_keyword_rank',
+            'dev_client_payment_type',
+            'dev_client_monthly',
+            'dev_client_perlead',
+            'dev_chat',
+            'dev_calls',
+            'dev_citations',
+            'dev_bl_citations',
+            'dev_bl_reviews',
+            'dev_citations',
+            'weather_enable',
+            'weather_api',
+            'weather_zip',
+            'dev_ontraport_enable',
+            'dev_ontraport_clientkey',
+            'dev_ontraport_apikey',
+            'dev_ontraport_appid',
+            'dev_servicetitan_apikey',
+        );
 
-	if ( count($_POST) > 0 && isset($_POST['gapi_override_settings']) )
-	{
-		$options = array (
-			'dev_industry_other',
-			'lead_appt',
-			'appt_sale',
-			'rep_percent',
-			'inst_percent',
-			'rep_avg_tix',
-			'inst_avg_tix',
-          			
-		);
-		
-		foreach ( $options as $opt )
-		{
-			delete_option ( 'gapi_'.$opt, $_POST[$opt] );
-			add_option ( 'gapi_'.$opt, $_POST[$opt] );	
-		}				 
-	}
+        foreach ($options as $opt) {
+            delete_option('gapi_' . $opt, $_POST[$opt]);
+            add_option('gapi_' . $opt, $_POST[$opt]);
+        }
+    }
 
+    if (count($_POST) > 0 && isset($_POST['gapi_date_settings'])) {
+        $options = array(
+            'calendar_start',
+            'calendar_end',
+            'calendar_diff',
+        );
 
-	if ( count($_POST) > 0 && isset($_POST['gapi_segment_settings']) )
-	{
-		$options = array (
-			'industry_business_unit',
-          			
-		);
-		
-		foreach ( $options as $opt )
-		{
-			delete_option ( 'gapi_'.$opt, $_POST[$opt] );
-			add_option ( 'gapi_'.$opt, $_POST[$opt] );	
-		}				 
-	}
+        foreach ($options as $opt) {
+            delete_option('gapi_' . $opt, $_POST[$opt]);
+            add_option('gapi_' . $opt, $_POST[$opt]);
+        }
+    }
 
+    if (count($_POST) > 0 && isset($_POST['gapi_override_settings'])) {
+        $options = array(
+            'dev_industry_other',
+            'lead_appt',
+            'appt_sale',
+            'rep_percent',
+            'inst_percent',
+            'rep_avg_tix',
+            'inst_avg_tix',
 
-	if ( count($_POST) > 0 && isset($_POST['gapi_metrics_dimensions']) )
-	{
-		$options = array (
-			'primary_dimension',
-			'secondary_dimension',
-			'max_results',
-          			
-		);
-		
-		foreach ( $options as $opt )
-		{
-			delete_option ( 'gapi_'.$opt, $_POST[$opt] );
-			add_option ( 'gapi_'.$opt, $_POST[$opt] );	
-		}				 
-	}
-	
-	if ( count($_POST) > 0 && isset($_POST['gapi_adwords_metrics_dimensions']) )
-	{
-		$options = array (
-			'adw_primary_dimension',
-			'adw_secondary_dimension',
-			'adw_max_results',
-          			
-		);
-		
-		foreach ( $options as $opt )
-		{
-			delete_option ( 'gapi_'.$opt, $_POST[$opt] );
-			add_option ( 'gapi_'.$opt, $_POST[$opt] );	
-		}				 
-	}	
+        );
 
-	if ( count($_POST) > 0 && isset($_POST['gapi_speed_metrics_dimensions']) )
-	{
-		$options = array (
-			'speed_primary_dimension',
-			'speed_secondary_dimension',
-			'speed_max_results',
-          			
-		);
-		
-		foreach ( $options as $opt )
-		{
-			delete_option ( 'gapi_'.$opt, $_POST[$opt] );
-			add_option ( 'gapi_'.$opt, $_POST[$opt] );	
-		}				 
-	}
-	
-	if ( count($_POST) > 0 && isset($_POST['gapi_ontraport_campaign_options']) )
-	{
-		$options = array (
-			'dev_campaign_group',
-			'dev_campaign_group_name',
-          			
-		);
-		
-		foreach ( $options as $opt )
-		{
-			delete_option ( 'gapi_'.$opt, $_POST[$opt] );
-			add_option ( 'gapi_'.$opt, $_POST[$opt] );	
-		}				 
-	}
-	
-	if ( count($_POST) > 0 && isset($_POST['gapi_st_sort_data']) )
-	{
-		$options = array (
-		    'dev_st_campaign_group',
-			'dev_st_campaign_group_name',
-			'st_max_results',
-          			
-		);
-		
-		foreach ( $options as $opt )
-		{
-			delete_option ( 'gapi_'.$opt, $_POST[$opt] );
-			add_option ( 'gapi_'.$opt, $_POST[$opt] );	
-		}				 
-	}	
+        foreach ($options as $opt) {
+            delete_option('gapi_' . $opt, $_POST[$opt]);
+            add_option('gapi_' . $opt, $_POST[$opt]);
+        }
+    }
 
-	add_menu_page('CSAnalytics', 'CSAnalytics', 'edit_pages', 'csanalytics-panel', 'csanalytics_panel', '', 3);
-	add_submenu_page('csanalytics-panel', 'Analytics Settings', 'Settings', 'manage_options', 'csanalytics-settings', 'csanalytics_settings');
-	  
+    if (count($_POST) > 0 && isset($_POST['gapi_segment_settings'])) {
+        $options = array(
+            'industry_business_unit',
+
+        );
+
+        foreach ($options as $opt) {
+            delete_option('gapi_' . $opt, $_POST[$opt]);
+            add_option('gapi_' . $opt, $_POST[$opt]);
+        }
+    }
+
+    if (count($_POST) > 0 && isset($_POST['gapi_metrics_dimensions'])) {
+        $options = array(
+            'primary_dimension',
+            'secondary_dimension',
+            'max_results',
+
+        );
+
+        foreach ($options as $opt) {
+            delete_option('gapi_' . $opt, $_POST[$opt]);
+            add_option('gapi_' . $opt, $_POST[$opt]);
+        }
+    }
+
+    if (count($_POST) > 0 && isset($_POST['gapi_adwords_metrics_dimensions'])) {
+        $options = array(
+            'adw_primary_dimension',
+            'adw_secondary_dimension',
+            'adw_max_results',
+
+        );
+
+        foreach ($options as $opt) {
+            delete_option('gapi_' . $opt, $_POST[$opt]);
+            add_option('gapi_' . $opt, $_POST[$opt]);
+        }
+    }
+
+    if (count($_POST) > 0 && isset($_POST['gapi_speed_metrics_dimensions'])) {
+        $options = array(
+            'speed_primary_dimension',
+            'speed_secondary_dimension',
+            'speed_max_results',
+
+        );
+
+        foreach ($options as $opt) {
+            delete_option('gapi_' . $opt, $_POST[$opt]);
+            add_option('gapi_' . $opt, $_POST[$opt]);
+        }
+    }
+
+    if (count($_POST) > 0 && isset($_POST['gapi_ontraport_campaign_options'])) {
+        $options = array(
+            'dev_campaign_group',
+            'dev_campaign_group_name',
+
+        );
+
+        foreach ($options as $opt) {
+            delete_option('gapi_' . $opt, $_POST[$opt]);
+            add_option('gapi_' . $opt, $_POST[$opt]);
+        }
+    }
+
+    if (count($_POST) > 0 && isset($_POST['gapi_st_sort_data'])) {
+        $options = array(
+            'dev_st_campaign_group',
+            'dev_st_campaign_group_name',
+            'st_max_results',
+
+        );
+
+        foreach ($options as $opt) {
+            delete_option('gapi_' . $opt, $_POST[$opt]);
+            add_option('gapi_' . $opt, $_POST[$opt]);
+        }
+    }
+
+    add_menu_page('CSAnalytics', 'CSAnalytics', 'edit_pages', 'csanalytics-panel', 'csanalytics_panel', '', 3);
+    add_submenu_page('csanalytics-panel', 'Analytics Settings', 'Settings', 'edit_users', 'csanalytics-settings', 'csanalytics_settings');
+
 }
 ?>
--- lib/inc/functions.php
+++ lib/inc/functions.php
@@ -1,39 +1,47 @@
 <?php
-	// CSAnalytics Scripts and Style
-	add_action('wp_enqueue_scripts', 'csanalytics_ext_scripts');
-	function csanalytics_ext_scripts()  {
-		if (!is_admin()) {
-			wp_register_script('js-cookie', plugins_url('js/js-cookie.js',__FILE__),array(), null, true);
-			wp_enqueue_script('js-cookie');			
-		}		
-	}
-	// Google Tag Manager Scripts
-	add_action( 'wp_head', 'csanalytics_gtagmgr_script' );
-	function csanalytics_gtagmgr_script() {
-	$dev_tag_mgr = get_option('gapi_dev_tag_mgr');
-        if ($dev_tag_mgr <> ""){
-            echo stripslashes ($dev_tag_mgr);
-        }
-	}
-	// Convirza DNI Scripts
-	add_action( 'wp_footer', 'csanalytics_dni_script' );
-	function csanalytics_dni_script() { 
-	    $dev_conv_profile = get_option('gapi_dev_conv_profile');
-            $dev_conv_script =  get_option('gapi_dev_conv_script');
-            if ($dev_conv_profile <> "" && $dev_conv_script <> ""){	
-	        wp_register_script( 'csanalytics-dni', 'https://dni.logmycalls.com/dni.js',array('js-cookie'), null, true);
-	        wp_enqueue_script('csanalytics-dni');
-                echo stripslashes ($dev_conv_script);
-			
-            }
-	}
-	
-	// Heatmap DNI Scripts
-	add_action( 'wp_head', 'csanalytics_heatmap_script' );
-	function csanalytics_heatmap_script() {
-	$dev_heatmap_script = get_option('gapi_dev_heatmap_script'); 
-        if ($dev_heatmap_script <> ""){
-            echo stripslashes ($dev_heatmap_script);
-        }
-	}	
-?>
+// CSAnalytics Scripts and Style
+add_action('wp_enqueue_scripts', 'csanalytics_ext_scripts');
+function csanalytics_ext_scripts() {
+    if (!is_admin()) {
+        wp_register_script('js-cookie', plugins_url('js/js-cookie.js', __FILE__), array(), null, true);
+        wp_enqueue_script('js-cookie');
+    }
+}
+// Google Tag Manager Scripts
+add_action('wp_head', 'csanalytics_gtagmgr_script');
+function csanalytics_gtagmgr_script() {
+    $dev_tag_mgr = get_option('gapi_dev_tag_mgr');
+    if ($dev_tag_mgr != "") {
+        echo stripslashes($dev_tag_mgr);
+    }
+}
+add_action('wp_footer', 'csanalytics_gtagmgr_script_footer');
+function csanalytics_gtagmgr_script_footer() {
+    $dev_tag_mgr_footer = get_option('gapi_dev_tag_mgr_footer');
+    if ($dev_tag_mgr_footer != "") {
+        echo stripslashes($dev_tag_mgr_footer);
+    }
+}
+
+// Convirza DNI Scripts
+add_action('wp_footer', 'csanalytics_dni_script');
+function csanalytics_dni_script() {
+    $dev_conv_profile = get_option('gapi_dev_conv_profile');
+    $dev_conv_script = get_option('gapi_dev_conv_script');
+    if ($dev_conv_profile != "" && $dev_conv_script != "") {
+        wp_register_script('csanalytics-dni', 'https://dni.logmycalls.com/dni.js', array('js-cookie'), null, true);
+        wp_enqueue_script('csanalytics-dni');
+        echo stripslashes($dev_conv_script);
+
+    }
+}
+
+// Heatmap DNI Scripts
+add_action('wp_head', 'csanalytics_heatmap_script');
+function csanalytics_heatmap_script() {
+    $dev_heatmap_script = get_option('gapi_dev_heatmap_script');
+    if ($dev_heatmap_script != "") {
+        echo stripslashes($dev_heatmap_script);
+    }
+}
+?>f
