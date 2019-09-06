7efd3d0e205674db45d8bfe83b4c154152ab1e67
Brian West <32143067+bdwest17@users.noreply.github.com>
2019-09-06 14:02:23 -0400

    Bump Version

--- main.php
+++ main.php
@@ -3,7 +3,7 @@
 Plugin Name: CSAnalytics
 Plugin URI: http://leadsnearby.com
 Description: Plugin to display LeadsNearby Metrics
-Version: 3.3.0
+Version: 3.3.1
 Author: LeadsNearby
 Author URI: http://leadsnearby.com
 License: GPLv2
